2897e2abcea02f48eb8a31ebac1db421ec2b4d1b
RainWell07 <117939087+RainWell07@users.noreply.github.com>
2023-04-08 16:35:21 +0300

    finished lessons

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const simpleContainer = document.querySelector(".gallery");
+const simpleMarkup = galleryItems.map(
+  ({ preview, description, original }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+);
+simpleContainer.insertAdjacentHTML("beforeend", simpleMarkup.join(""));
 
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionsData: "alt",
+});
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = form.querySelector('input[name = "email"]');
+const message = form.querySelector('textarea[name = "message"]');
+
+
+
+const StateForm = throttle(() => {
+const state = {
+    email: input.value,
+    message: message.value, 
+};
+    localStorage.setItem('feedback-form-state', JSON.stringify(state));
+},  500);
+
+
+form.addEventListener('input', StateForm);
+const SavedTime = localStorage.getItem('feedback-form-state');
+
+if (SavedTime) {
+    const state = JSON.parse(SavedTime);
+    input.value = state.email;
+    message.value = state.message;
+}
+
+
+form.addEventListener('submit', (evt) => {
+evt.preventDefault()
+const state = {
+    email: input.value,
+    message: message.value,
+};
+console.log(state);
+
+localStorage.removeItem('feedback-form-state');
+    input.value = "";
+    message.value = "";
+});
